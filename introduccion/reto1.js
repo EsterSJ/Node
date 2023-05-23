711556e237444df57776eb42bb26ebd855b79d6e
EsterSJ <ester_s@hotmail.com>
2023-05-23 12:17:12 +0200

    reto2

--- introduccion/reto1.js
+++ introduccion/reto1.js
@@ -3,4 +3,4 @@ console.log("Mensaje 1");
 setTimeout(()=>{
     console.log("Mensaje 2");
     console.log("Mensaje 3");
-}, 2000)
+}, 3000)
--- introduccion/reto2.js
+++ introduccion/reto2.js
@@ -1,5 +1,23 @@
+const fs = require ('fs');
+
 let person = {
     name: "Ester",
     surname: "Sanchez",
     age: 38
 }
+
+//escritura del objeto en el fichero .json
+fs.writeFile("reto2.json", JSON.stringify(person), (err) => {
+    if (err) throw err
+    else {
+        //lectura del fichero .json en caso de haberse grabado correctamente
+        console.log("Fichero grabado correctamente");
+        fs.readFile('reto2.json', (err, data) => {
+            if (err) throw err;
+            //mostrar datos del fichero por pantalla
+            console.log("Datos leidos del fichero JSON => " + data);
+            //dejar el fichero .json de nuevo vacio
+            fs.writeFile("reto2.json", "", (err) => { if (err) throw err });
+        });
+    }
+  });
